97a50fdf155490315603e4449788dcc1f5bf3894
lifeincha0s <32803552+lifeincha0s@users.noreply.github.com>
2017-12-02 15:30:18 -0500

    Add files via upload

---
+++ web_survey/scripts/javascript.js
@@ -0,0 +1,97 @@
+/*
+ * 	form validation functions
+ */
+function is_empty(textbox, alertbox) {
+	with(textbox) {
+		if (value==null || value=="") {
+			if (alertbox!="") {
+				alert(alertbox);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+function login_validation(form) {
+	if (is_empty(form.user_name, "'UserID' cannot be empty!")) {
+		form.user_name.focus();
+		return false;
+	};
+	if (is_empty(form.user_pass, "'Password' cannot be empty!")) {
+		form.user_pass.focus();
+		return false;
+	};
+}
+
+function send_email_validation(form) {
+	if (is_empty(form.email_address, "'Email Address' cannot be empty!")) {
+		form.email_address.focus();
+		return false;
+	};
+	if (is_empty(form.email_subject, "'Subject' cannot be empty!")) {
+		form.email_subject.focus();
+		return false;
+	};
+	if (is_empty(form.email_body, "'Text Body' cannot be empty!")) {
+		form.email_body.focus();
+		return false;
+	};
+}
+
+function create_account_validation(form) {
+	if (is_empty(form.contact_name, "'Contact Name' cannot be empty!")) {
+		form.contact_name.focus();
+		return false;
+	};
+	if (is_empty(form.contact_email, "'User Email' cannot be empty!")) {
+		form.contact_email.focus();
+		return false;
+	};
+	if (is_empty(form.user_name, "'LogonID' cannot be empty!")) {
+		form.user_name.focus();
+		return false;
+	};
+	if (is_empty(form.password1, "'Password' cannot be empty!")) {
+		form.password1.focus();
+		return false;
+	};
+	if (is_empty(form.password2, "'Verify Password' cannot be empty!")) {
+		form.password2.focus();
+		return false;
+	};
+	if (form.password1.value != form.password2.value) {
+		alert("Passwords do not match!");
+		form.password1.value="";
+		form.password2.value="";
+		form.password1.focus();
+		return false;
+	};
+}
+
+function reset_password_validation(form) {
+	if (is_empty(form.user_name, "'UserID' cannot be empty!")) {
+		form.user_name.focus();
+		return false;
+	};
+	if (is_empty(form.contact_email, "'Email Address' cannot be empty!")) {
+		form.user_email.focus();
+		return false;
+	};
+}
+function form_validation(thisform) {
+	if (thisform.name == "form_login") {
+		return login_validation(thisform);
+	};
+	if (thisform.name == "form_send_email") {
+		return send_email_validation(thisform);
+	};
+	if (thisform.name == "form_reset_password") {
+		return reset_password_validation(thisform);
+	};
+	if (thisform.name == "form_create_account") {
+		return create_account_validation(thisform);
+	};
+	return false;
+}
