e824330977fa4c4294e21b0b016ef6798c02d2bd
shaw9825 <32803626+shaw9825@users.noreply.github.com>
2017-12-03 20:28:49 -0500

    Update LoadDBLocal.php

--- LoadDBLocal.php
+++ LoadDBLocal.php
@@ -1,15 +1,28 @@
 <?php
 
+$sql = "Select * FROM distribution_list WHERE distribution_KEY='".$distroKey."'";
+$tmp = $dbh->prepare($sql);
+$tmp-> execute();
+$distro = $tmp->fetchALL(\PDO::FETCH_ASSOC);
 
-$sql = "Select * FROM category";
+
+$survey_ID = $distro[0][survey_ID];
+$accountID = $distro[0][user_ID];
+
+
+$sql = "Select * FROM category WHERE survey_ID='".$survey_ID."'";
   $tmp = $dbh->prepare($sql);
 $tmp-> execute();
 $category = $tmp->fetchALL(\PDO::FETCH_ASSOC);
 $num_cats = count($category);
 
-$sql = "Select * FROM question";
+
+
+$sql = "Select * FROM question WHERE survey_ID='".$survey_ID."'";
   $tmp = $dbh->prepare($sql);
 $tmp-> execute();
 $question = $tmp->fetchALL(\PDO::FETCH_ASSOC);
 $num_questions = count($question);
+
+
 ?>
