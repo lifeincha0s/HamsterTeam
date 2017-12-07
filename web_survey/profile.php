09bf4997233f34fcc9cdeb7b9bd9c37f8d1b8e3a
lifeincha0s <32803552+lifeincha0s@users.noreply.github.com>
2017-12-06 20:59:55 -0500

    Create profile.php

---
+++ web_survey/profile.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * 	profile.php
+*/
+include('./session.php');	// always include: this carries session cookie
+$page_title = "Profile: " . $login_session;
+?>
+<!DOCTYPE HTML PUBLIC>
+<html>
+
+<head>
+	<title><?php echo $page_title; ?></title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<script src="./scripts/javascript.js"></script>
+</head>
+
+<body>
+<div id="profile">
+	<b id="welcome">Welcome : <a href="./profile.php"><i><?php echo $login_session; ?></i></a></b>
+	<b id="password"><a href="./change_password.php">Change Password</a></b>
+	<b id="logout"><a href="./logout.php">Log Out</a></b>
+</div>
+</body>
+
+</html>
