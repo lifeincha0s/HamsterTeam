ea53379060c93ad2bb1cd19d9598f1e2653a32b4
lifeincha0s <32803552+lifeincha0s@users.noreply.github.com>
2017-12-07 19:43:35 -0500

    Update session.php

--- web_survey/session.php
+++ web_survey/session.php
@@ -7,16 +7,16 @@ session_start();							// Start Session
 require_once('./dbConnection.php');
 
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$pdo = new dbConnection();
+$pdo	= new dbConnection();
 
 // SQL Query To Fetch Complete Information Of User
-$sql = "SELECT * FROM admin_credentials WHERE admin_name= ? LIMIT 1";
-$query = $pdo->prepare($sql);
+$sql	= "SELECT * FROM admin_credentials WHERE admin_name= ? LIMIT 1";
+$query	= $pdo->prepare($sql);
 $query->bindParam(1, $_SESSION['valid_user']);
 $query->execute();
 
 // Get results
-$row = $query->fetch(PDO::FETCH_ASSOC);
+$row	= $query->fetch(PDO::FETCH_ASSOC);
 $query->closeCursor();					// Close dbConnection
 
 $login_session = $row['admin_name'];
